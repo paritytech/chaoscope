e59634fce70fc349df2522c69f65a5de75e03287
Bernardo A. Rodrigues <bernardo@parity.io>
2021-11-08 20:18:49 -0300

    test_setup.sh conditionals

--- test_setup.sh
+++ test_setup.sh
@@ -17,32 +17,63 @@ echo ""
 echo "Our local setup will be based on:"
 echo " - [substrate-node-template](https://github.com/substrate-developer-hub/substrate-node-template)"
 echo " - [pallet-chaos](https://github.com/paritytech/pallet-chaos)"
-
 echo ""
-echo "Let's clone substrate-node-template..."
-
-rm -rf substrate-node-template
-git clone https://github.com/substrate-developer-hub/substrate-node-template -b v${SUBSTRATE_V}+${SUBSTRATE_TIMESTAMP} substrate-node-chaos
-pushd substrate-node-chaos
-
+echo "Please, set your options as environment variables and call this script again."
+echo "START_NODE=1               -> Spin a substrate-node-chaos node (as screen session)"
+echo "RUN_CHAOSCOPE=1          -> Run chaoscope cli"
+echo "BUILD_CHAOSCOPE_WASM=1               -> Build chaoscope.wasm"
 echo ""
-echo "Let's clone pallet-chaos..."
-pushd pallets
-git clone ssh://git@github.com/paritytech/pallet-chaos.git
-popd
-
+echo "ex.: START_NODE=1 RUN_CHAOSCOPE=1 ./test_setup.sh"
 echo ""
-echo "Let's add pallet-chaos to the runtime..."
-git apply pallets/pallet-chaos/diff/add_chaos_runtime.diff
 
-echo ""
-echo "Let's build the node-template executable..."
-cargo build --release -j7
+if [ ! -d "substrate-node-chaos" ]; then
+  echo ""
+  echo "Let's clone substrate-node-template..."
 
-echo ""
-echo "Let's start the node-template executable..."
-echo "From now on, you should turn to [chaoscope](https://github.com/paritytech/chaoscope) for RPC instrumentation."
-./target/release/node-template --dev --tmp
+  git clone https://github.com/substrate-developer-hub/substrate-node-template -b v${SUBSTRATE_V}+${SUBSTRATE_TIMESTAMP} substrate-node-chaos
+  pushd substrate-node-chaos
 
+  echo ""
+  echo "Let's clone pallet-chaos..."
+  pushd pallets
+  git clone ssh://git@github.com/paritytech/pallet-chaos.git
+  popd
 
+  echo ""
+  echo "Let's add pallet-chaos to the runtime..."
+  git apply pallets/pallet-chaos/diff/add_chaos_runtime.diff
+  popd
+fi
 
+if [ ! -d "substrate-node-chaos/target/release" ]; then
+  echo ""
+  echo "Let's build the node-template executable..."
+  pushd substrate-node-chaos
+  cargo build --release
+  popd
+fi
+
+if [ ! -z "$START_NODE" ]; then
+  echo ""
+  echo "Let's start the substrate-node-chaos executable..."
+  pushd substrate-node-chaos
+  screen -d -m ./target/release/node-template --dev --tmp
+
+  echo "Your substrate-node-chaos is running as a screen session in the background."
+  echo "You can check it by running \"ps aux | grep node-template\""
+  echo "You can kill it by running \"sudo killall node-template\"."
+  popd
+fi
+
+if [ ! -z "$RUN_CHAOSCOPE" ]; then
+  echo ""
+  echo "Let's run the chaoscope cli..."
+
+  cargo run --release
+fi
+
+if [ ! -z "$BUILD_CHAOSCOPE_WASM" ]; then
+  echo ""
+  echo "Let's build chaoscope.wasm..."
+  # ToDo
+fi
