754d3cb9d3ea45aa88e6857b3aabcc926362f528
Bernardo A. Rodrigues <bernardo@parity.io>
2021-11-08 23:47:03 -0300

    warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    println!("          _");
+    println!("         ⇖⇑⇗");
+    println!("         ⇐●⇒");
+    println!("         ⇙⇓⇘");
+    println!("          ‾");
+    println!("⚠️Expect... Chaoscope! ⚠");
+    println!("Open http://localhost:3030 on your browser.");
+
+    warp::serve(warp::fs::dir("www"))
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
