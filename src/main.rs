b045d3d446e91bbdf9be87474bab518aaa54d8dc
Bernardo A. Rodrigues <bernardo@parity.io>
2021-11-08 23:50:30 -0300

    shrust

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![deny(warnings)]
 
+extern crate shrust;
+use shrust::{Shell, ShellIO};
+use std::io::prelude::*;
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
@@ -12,7 +16,15 @@ async fn main() {
     println!("⚠️Expect... Chaoscope! ⚠");
     println!("Open http://localhost:3030 on your browser.");
 
-    warp::serve(warp::fs::dir("www"))
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    // warp::serve(warp::fs::dir("www"))
+    //     .run(([127, 0, 0, 1], 3030))
+    //     .await;
+
+    let mut shell = Shell::new(());
+    shell.new_command_noargs("hello", "Say 'hello' to the world", |io, _| {
+        writeln!(io, "Hello World !!!")?;
+        Ok(())
+    });
+
+    shell.run_loop(&mut ShellIO::default());
 }
