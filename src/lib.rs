19ad94bb8bd5fc4ce8007d096bbab5f441b7a3f5
Bernardo A. Rodrigues <bernardo@parity.io>
2021-12-02 21:56:01 -0300

    chaoscope.sh block timing

--- src/lib.rs
+++ src/lib.rs
@@ -17,18 +17,41 @@
 // chaoscope RPC calls
 
 use sp_keyring::AccountKeyring;
+use std::time::Duration;
 use subxt::{ClientBuilder, PairSigner};
 
 #[subxt::subxt(runtime_metadata_path = "metadata/substrate-node-chaos.scale")]
 pub mod chaosrpc {}
 
 pub async fn rpc_drag_block_unit_weight(n: u64) -> Result<(), Box<dyn std::error::Error>> {
+    // alice signer
     let signer = PairSigner::new(AccountKeyring::Alice.pair());
+
+    // runtime API
     let api = ClientBuilder::new()
         .build()
         .await?
         .to_runtime_api::<chaosrpc::RuntimeApi<chaosrpc::DefaultConfig>>();
 
+    // block subscriber
+    let mut sub = api.client.rpc().subscribe_blocks().await.unwrap();
+
+    // get head block hash
+    let head_block_hash = loop {
+        if let Ok(Some(block)) = sub.next().await {
+            break block.hash();
+        }
+    };
+
+    // get head block timestamp
+    let head_block_before = api
+        .storage()
+        .timestamp()
+        .now(Some(head_block_hash))
+        .await
+        .unwrap();
+
+    // submit extrinsic
     let result = api
         .tx()
         .chaos()
@@ -36,13 +59,36 @@ pub async fn rpc_drag_block_unit_weight(n: u64) -> Result<(), Box<dyn std::error
         .sign_and_submit_then_watch(&signer)
         .await?;
 
+    // check event
+    if result
+        .find_event::<chaosrpc::chaos::events::Stalled>()?
+        .is_none()
+    {
+        panic!("Event not found!");
+    }
+
+    // get head block timestamp
+    let head_block_hash = loop {
+        if let Ok(Some(block)) = sub.next().await {
+            break block.hash();
+        }
+    };
+
+    // get head block timestamp
+    let head_block_after = api
+        .storage()
+        .timestamp()
+        .now(Some(head_block_hash))
+        .await
+        .unwrap();
+
+    // calculate block time (after - before)
+    let block_time = head_block_after.checked_sub(head_block_before).unwrap();
+    let d = Duration::from_millis(block_time);
+
     // get fees paid
 
-    if let Some(event) = result.find_event::<chaosrpc::chaos::events::Stalled>()? {
-        println!("drag_block_unit_weight success: n: {:?}", event.0);
-    } else {
-        println!("drag_block_unit_weight failed");
-    }
+    println!("n: {}, block execution time: {} ms", n, d.as_millis());
 
     Ok(())
 }
